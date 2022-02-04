ac23e6530257e90875d05d00b3a2ab4f0d998f04
Pon Farreny <josep.pon-farreny@siemens.com>
2022-02-04 16:58:56 +0100

    Initial commit

---
+++ paraulogic.js
@@ -0,0 +1,150 @@
+
+let CHARACTERS_TO_IGNORE = ["·", "-", "'", "’"];
+
+/* global variable to keep words data */
+var wordsByCharacter = null;
+var wordsAffixes = null
+
+function stripDiacritics(text) {
+    return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+function isAnIgnoredCharacter(char) {
+    return /\s/.test(char) || CHARACTERS_TO_IGNORE.includes(char);
+}
+
+function isWordValid(word, validCharacters) {
+    for (let i = 0; i < word.length; ++i) {
+        if (
+            !isAnIgnoredCharacter(word[i])
+            && !validCharacters.includes(word[i])
+        ) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function postProcessWords(wordsData) {    
+    wordsByCharacter = {}
+    wordsAffixes = {}    
+
+    wordsData.forEach(entry => {
+        // Separate full words from affixes
+        let affixes = [];
+        let words = [];
+        entry.forEach(item => {
+            if (item.startsWith("-") || item.endsWith("-")) {
+                affixes.push(item);
+            } else {
+                words.push(item);
+            }
+        });
+
+        words.forEach(word => {
+            let plainWord = stripDiacritics(word);
+            wordsAffixes[plainWord] = affixes;
+            for (let i = 0; i < plainWord.length; ++i) {
+                let ch = plainWord[i];
+
+                if (!isAnIgnoredCharacter(ch)) {
+                    if (!(ch in wordsByCharacter)) {
+                        wordsByCharacter[ch] = [plainWord];
+                    } else {
+                        wordsByCharacter[ch].push(plainWord);
+                    }
+                }
+            }
+        });
+    });
+
+    for (const key in wordsByCharacter) {
+        wordsByCharacter[key] = [... new Set(wordsByCharacter[key])];
+        wordsByCharacter[key].sort();
+    }
+}
+
+function load_words(onloaded) {
+    if (wordsByCharacter === null) {
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open('GET', 'data/words.json', true);
+        xobj.onreadystatechange = function () {
+            if (xobj.readyState == 4 && xobj.status == "200") {
+                let jsonData = JSON.parse(xobj.responseText);
+                postProcessWords(jsonData["words"]);
+                onloaded();
+            }
+        };
+        xobj.send();
+    } else {
+        onloaded();
+    }
+}
+
+function getCharacters() {
+    let domElements = Array.from(document.getElementsByClassName("hex-input"));
+    return domElements.map(element => element.value.length > 0 ? element.value[0].toLowerCase() : "");
+}
+
+
+function getRequiredCharacter() {
+    let domElement = document.getElementById("center-letter-input");
+    if (domElement.value.length > 0) {
+        return domElement.value[0].toLowerCase();
+    }
+    return "";
+}
+
+
+function findSolutions() {
+    let characters = getCharacters();
+    let keyCharacter = getRequiredCharacter();
+
+
+    let keyWords = keyCharacter in wordsByCharacter
+        ? wordsByCharacter[keyCharacter]
+        : [];
+    let validWords = keyWords.filter(
+        x => x.length > 2 && isWordValid(x, characters));
+
+    let validAffixes = {};
+    validWords.forEach(word => {
+        validAffixes[word] = []
+        wordsAffixes[word].forEach(affix => {
+            if (isWordValid(affix, characters)) {
+                validAffixes[word].push(affix);
+            }
+        })        
+    });
+
+    return [validWords, validAffixes];
+}
+
+
+document.getElementById("solve-button").onclick = function () {
+    load_words(function () {
+        let resultsDiv = document.getElementById("definition");
+        const [validWords, validAffixes] = findSolutions();
+
+        // Clear DOM
+        resultsDiv.innerHTML = "";
+
+        // Set solutions
+        validWords.forEach(word => {
+            const para = document.createElement("p");
+            const affixesText = validAffixes[word].join(" | ");
+            if (validAffixes[word].length > 0) {
+                const node = document.createTextNode(
+                    word + " | " + affixesText);
+                para.appendChild(node);
+            } else {
+                const node = document.createTextNode(word);
+                para.appendChild(node);
+            }
+
+            resultsDiv.appendChild(para);
+        })
+
+    });
+}
